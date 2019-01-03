e1db4b0cd9f6c31e7110651f383576c04de2489c
Frank Wan <frankwan27@gmail.com>
2019-01-03 11:07:34 -0800

    Basic hand and drag system

---
+++ Assets/Draggable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private Transform handParent = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        handParent = this.transform.parent;
+        this.transform.SetParent(handParent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+        this.transform.SetParent(handParent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+
+
+
+}
